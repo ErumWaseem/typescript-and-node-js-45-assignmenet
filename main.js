536d7f873abd5023d1ad992ee5327401d9aa9242
Erum Waseem <erumw46@gmail.com>
2024-05-20 22:57:27 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function cityCountry(city, country) {
+    return "".concat(city, ", ").concat(country);
+}
+var C1 = cityCountry('Lahore', 'Pakistan');
+var C2 = cityCountry('Tokyo', 'Japan');
+var C3 = cityCountry('Paris', 'France');
+console.log(C1);
+console.log(C2);
+console.log(C3);
---
+++ main.ts
@@ -0,0 +1,11 @@
+function cityCountry(city: string, country: string): string {
+   return`${city}, ${country}`
+}
+
+let C1 = cityCountry('Lahore','Pakistan')
+let C2 = cityCountry('Tokyo','Japan')
+let C3 = cityCountry('Paris','France')
+
+console.log(C1)
+console.log(C2)
+console.log(C3)
